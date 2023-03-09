7bb07178f61128f7ab87679bb08deaecb63d8454
manhhung <hunter@tweetworldtravel.com>
2023-03-09 10:01:51 +0700

    new code

--- test2.py
+++ test2.py
@@ -1,29 +1,16 @@
-import cv2
-import os
-import datetime
+import cv2 as cv
 
+gst = "rtspsrc location=rtsp://usser:pass@ip:port/url latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink"
+capture = cv.VideoCapture(gst, cv.CAP_GSTREAMER)
 
-def save_frame_camera_cycle(device_num, dir_path, basename, cycle, ext='jpg', delay=1, window_name='frame'):
-    cap = cv2.VideoCapture(device_num)
+print("Is pipeline open: ", capture.isopened())
+while capture.isOpened() :
+     ret, frame = capture.read()
+     print("Is receiving frames: ", ret)
+     cv.imshow("Stream", frame)
+     
+     if cv.waitKey(25) & 0xFF == ord("q"):
+          break
 
-    if not cap.isOpened():
-        return
-
-    os.makedirs(dir_path, exist_ok=True)
-    base_path = os.path.join(dir_path, basename)
-
-    n = 0
-    while True:
-        ret, frame = cap.read()
-        cv2.imshow(window_name, frame)
-        if cv2.waitKey(delay) & 0xFF == ord('q'):
-            break
-        if n == cycle:
-            n = 0
-            cv2.imwrite('{}_{}.{}'.format(base_path, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'), ext), frame)
-        n += 1
-
-    cv2.destroyWindow(window_name)
-
-
-save_frame_camera_cycle(0, 'data/temp', 'camera_capture_cycle', 300)
+capture.release()
+cv.destroyAllWindows()
